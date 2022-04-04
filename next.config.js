93a0fb66dc9ea147e870ec4bc85093bc5a75939c
Dmitriy Sidorenko <dmitriysidor3nk0@gmail.com>
2022-04-04 21:00:37 +0300

    envs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const { parsed: localEnv } = require("dotenv").config();
 
 module.exports = {
-  env: { API_KEY: process.env.SHOPIFY_API_KEY },
+  env: {
+    API_KEY: process.env.SHOPIFY_API_KEY,
+    NEXT_PUBLIC_WIDGET_PATH: process.env.NEXT_PUBLIC_WIDGET_PATH,
+    NEXT_PUBLIC_PREVIEW_WIDGET_PATH:
+      process.env.NEXT_PUBLIC_PREVIEW_WIDGET_PATH,
+  },
   webpack: (config) => {
     // Add ESM support for .mjs files in webpack 4
     config.module.rules.push({
